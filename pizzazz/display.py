5faa4c6f6ef1783cd8b962da5048b514a982792d
Justin Swanson <geeksunny@gmail.com>
2016-12-11 21:11:45 -0600

    Stubbed out some abstraction for future displays

--- pizzazz/display.py
+++ pizzazz/display.py
@@ -1,20 +1,36 @@
 from oled.device import ssd1306
 from oled.render import canvas
+from utils import not_implemented
 
 
-class SSD1306(object):
+class AbstractI2CScreen(object):
 
-    # TODO: Should some of this be broken out into an AbstractI2CScreen class?
-
-    SCREEN_WIDTH = 128
-    SCREEN_HEIGHT = 64
-    FILL_SOLID = 255
-    FILL_EMPTY = 0
+    # TODO: Work in more of a common interface that uses the PIL objects sent to a master draw() method
 
     def __init__(self, i2c_address, i2c_port=1):
         self.address = i2c_address
         self.port = i2c_port
         self._device = self._request_device()
+
+    def _request_device(self):
+        raise NotImplemented(not_implemented(self, "_request_device()"))
+
+    def clear_screen(self):
+        raise NotImplemented(not_implemented(self, "clear_screen()"))
+
+    def screen_width(self):
+        raise NotImplemented(not_implemented(self, "screen_width()"))
+
+    def screen_height(self):
+        raise NotImplemented(not_implemented(self, "screen_height()"))
+
+
+class SSD1306(AbstractI2CScreen):
+
+    SCREEN_WIDTH = 128
+    SCREEN_HEIGHT = 64
+    FILL_SOLID = 255
+    FILL_EMPTY = 0
 
     def _request_device(self):
         return ssd1306(port=self.port, address=self.address)
@@ -23,4 +39,11 @@ class SSD1306(object):
         with canvas(self._device) as draw:
             # TODO: Work this in to the managed UI system
             draw.rectangle((0, 0, self.SCREEN_WIDTH, self.SCREEN_HEIGHT), fill=0)
+
+    def screen_height(self):
+        return self.SCREEN_HEIGHT
+
+    def screen_width(self):
+        return self.SCREEN_WIDTH
+
 
---
+++ pizzazz/utils.py
@@ -0,0 +1,2 @@
+def not_implemented(clazz, method_name):
+    return "Class {} does not implement {}".format(clazz.__class__.__name__, method_name)
